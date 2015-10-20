66f01add2438ad7928024fe7c6b885494ceea921
jichaofdu <chaojifudan@outlook.com>
2015-10-20 08:41:43 +0800

    Final Commit

--- src/databaseInterface/Run.java
+++ src/databaseInterface/Run.java
@@ -5,30 +5,32 @@ import java.sql.SQLException;
 public class Run {
 	public static void main(String[] args){
 		ReadSqlLine readSql = new ReadSqlLine();
-//		readSql.readSqlFromTxt("E:\\数据库技术\\DB_LAB1\\RoomTableCreate.txt");
-//		readSql.executeSql();
-//		ReadAccess a = new ReadAccess();
-//		a.readDataFromStudent();
-//		ReadExcel a = new ReadExcel("room");
-//		try {
-//			a.saveToDatabaseRoom();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		readSql.readSqlFromTxt("E:\\数据库技术\\DB_LAB1\\StudentTableCreate.txt");
-		readSql.executeSql();		
+		readSql.readSqlFromTxt("E:\\数据库技术\\DB_LAB1\\RoomTableCreate.txt");
+		readSql.executeSql();
+		ReadAccess a = new ReadAccess();
+		a.readDataFromRoom();
+		//ReadExcel a = new ReadExcel("room");
+			try {
+				a.saveRoom();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+//		readSql.readSqlFromTxt("E:\\数据库技术\\DB_LAB1\\StudentTableCreate.txt");
+//		readSql.executeSql();		
 //		ReadExcel a = new ReadExcel("student");
 //		try {
 //			a.saveToDatabaseStudent();
 //		} catch (Exception e) {
 //			e.printStackTrace();
 //		}
-		ReadExcel a = new ReadExcel("student");
-		try {
-			a.saveToDatabaseStudent();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		ReadExcel a = new ReadExcel("student");
+//		try {
+//			a.saveToDatabaseStudent();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 
 
 		
